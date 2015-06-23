88a6e16019c46b05be5ff4bb1a41941b0b956735
kayleyang <627134000@qq.com>
2015-06-23 20:23:48 +0800

    改善用户交互、完善代码、对配置文件的中文支持

--- solve.py
+++ solve.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding:utf-8 -*-
 
 import sys
 reload(sys)
@@ -32,7 +32,9 @@ class filterExcel(object):
         self.data = []
 
     def initExcel(self):
+        print u'正在查找',self.inputFileName
         if os.path.exists(self.inputFileName):
+            print u'找到要处理的Excel文件'
             self.readExcel(self.inputFileName)
             self.execExcel()
         else:
@@ -84,7 +86,7 @@ class filterExcel(object):
             execExcel()
 
     def readExcel(self, fileName):
-        os.system('cls')
+        # os.system('cls')
         print u"正在读取",fileName,u"文件"
         excel = xlrd.open_workbook(fileName)
         sheet = excel.sheet_by_name(u'Sheet1')
@@ -105,6 +107,8 @@ class filterExcel(object):
         data.append(self.filterTitle)
         titleLen = len(self.filterTitle)
         for k in range(titleLen,ncols):
+            # print len(self.filterCountName) ,self.data[0][k] , self.filterCountName
+            # sys,exit()
             if (self.filterCountTitle in self.data[0][k]) and ((len(self.filterCountName) <= 0) or (self.data[0][k] in self.filterCountName)):
                 data[0].append(self.data[0][k])
         fullTitle = self.data[0]
@@ -142,7 +146,6 @@ class filterExcel(object):
         sheet = excel.add_sheet(u'Sheet1')
         if os.path.exists(self.outputFileName):
             os.remove(self.outputFileName)
-        # print data
         for i, row in enumerate(data):
             # print row
             for j, cell in enumerate(row):
@@ -161,9 +164,9 @@ class filterExcel(object):
         open(path, 'w').write(content)  
 
         self.conf.read(path)
-        # options = self.conf.options('pathConfig')
+        # options = self.conf.options(u"路径配置".encode('utf-8'))
         # print 'options:', options
-        # items = self.conf.items('pathConfig')
+        # items = self.conf.items(u"路径配置".encode('utf-8'))
         # print 'items:', items
     
     def initFileList(self, dir, fileList, fileType):
@@ -218,6 +221,7 @@ class filterExcel(object):
             print u'发现了一个配置文件：',iniList[0]
             self.readConf(iniList[0])
             self.loadConf()
+            self.printSysConf()
             # TODO
         else :
             print u'发现了很多配置文件，请选一个吧'
@@ -228,55 +232,61 @@ class filterExcel(object):
                 if choosed < len(iniList):
                     self.readConf(iniList[0])
                     self.loadConf()
+                    self.printSysConf()
                     break
                 else :
                     print u'请重新输入:',
             # TODO
 
     def createSysConf(self):
-        self.conf.add_section('pathConfig')
-        self.conf.set('pathConfig', 'inputDir', '.')
-        self.conf.set('pathConfig', 'outputDir', '.')
-        self.conf.set('pathConfig', 'inputFileName', '.')
-        self.conf.set('pathConfig', 'outputFileName', '.')
+        self.conf.add_section(u"路径配置".encode('utf-8'))
+        self.conf.set(u"路径配置".encode('utf-8'), 'inputDir', '.')
+        self.conf.set(u"路径配置".encode('utf-8'), 'outputDir', '.')
+        self.conf.set(u"路径配置".encode('utf-8'), 'inputFileName', '.')
+        self.conf.set(u"路径配置".encode('utf-8'), 'outputFileName', self.outputFileName)
 
-        self.conf.add_section('filterConfig')
-        self.conf.set('filterConfig', 'filterTitle', self.listToString(self.filterTitle))
-        self.conf.set('filterConfig', 'filterBuyerTitle', self.filterBuyerTitle)
-        self.conf.set('filterConfig', 'filterBuyerName', self.listToString(self.filterBuyerName))
-        self.conf.set('filterConfig', 'filterBrandTitle', self.filterBrandTitle)
-        self.conf.set('filterConfig', 'filterBrandName', self.listToString(self.filterBrandName))
-        self.conf.set('filterConfig', 'filterCountTitle', self.filterCountTitle)
-        self.conf.set('filterConfig', 'filterCountName', self.listToString(self.filterCountName))
+        self.conf.add_section(u"过滤配置".encode('utf-8'))
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterTitle', self.listToString(self.filterTitle))
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterBuyerTitle', self.filterBuyerTitle)
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterBuyerName', self.listToString(self.filterBuyerName))
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterBrandTitle', self.filterBrandTitle)
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterBrandName', self.listToString(self.filterBrandName))
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterCountTitle', self.filterCountTitle)
+        self.conf.set(u"过滤配置".encode('utf-8'), 'filterCountName', self.listToString(self.filterCountName))
 
     def loadConf(self):
-        self.inputDir = self.conf.get('pathConfig', 'inputDir')
-        self.outputDir = self.conf.get('pathConfig', 'outputDir')
-        self.inputFileName = self.conf.get('pathConfig', 'inputFileName')
-        self.outputFileName = self.conf.get('pathConfig', 'outputFileName')
+        self.inputDir = (self.conf.get(u'路径配置'.encode('utf-8'), 'inputDir')).decode('utf-8')
+        self.outputDir = (self.conf.get(u'路径配置'.encode('utf-8'), 'outputDir')).decode('utf-8')
+        self.inputFileName = (self.conf.get(u'路径配置'.encode('utf-8'), 'inputFileName')).decode('utf-8')
+        outputFileName = (self.conf.get(u'路径配置'.encode('utf-8'), 'outputFileName')).decode('utf-8')
+        if outputFileName != '':
+            self.outputFileName = outputFileName
 
-        self.filterTitle = self.stringToList(self.conf.get('filterConfig', 'filterTitle'))
-        self.filterBuyerTitle = self.conf.get('filterConfig', 'filterBuyerTitle')     
-        self.filterBuyerName = self.stringToList(self.conf.get('filterConfig', 'filterBuyerName'))
-        self.filterBrandTitle = self.conf.get('filterConfig', 'filterBrandTitle')
-        self.filterBrandName = self.stringToList(self.conf.get('filterConfig', 'filterBrandName'))
-        self.filterCountTitle = self.conf.get('filterConfig', 'filterCountTitle')
-        self.filterCountName = self.stringToList(self.conf.get('filterConfig', 'filterCountName'))  
+        self.filterTitle = self.stringToList((self.conf.get(u"过滤配置".encode('utf-8'), 'filterTitle')).decode('utf-8'))
+        self.filterBuyerTitle = (self.conf.get(u"过滤配置".encode('utf-8'), 'filterBuyerTitle')).decode('utf-8')    
+        self.filterBuyerName = self.stringToList((self.conf.get(u"过滤配置".encode('utf-8'), 'filterBuyerName')).decode('utf-8'))
+        self.filterBrandTitle = (self.conf.get(u"过滤配置".encode('utf-8'), 'filterBrandTitle')).decode('utf-8')
+        self.filterBrandName = self.stringToList((self.conf.get(u"过滤配置".encode('utf-8'), 'filterBrandName')).decode('utf-8'))
+        self.filterCountTitle = (self.conf.get(u"过滤配置".encode('utf-8'), 'filterCountTitle')).decode('utf-8')
+        filterCountName = (self.conf.get(u"过滤配置".encode('utf-8'), 'filterCountName')).decode('utf-8')
+        if filterCountName != '':
+            self.filterCountName = self.stringToList(filterCountName)
 
     def printSysConf(self):
-        print u'[pathConfig]'
-        print u'inputDir = ',self.conf.get('pathConfig', 'inputDir')
-        print u'outputDir = ',self.conf.get('pathConfig', 'outputDir')
-        print u'inputFileName = ',self.conf.get('pathConfig', 'inputFileName')
-        print u'outputFileName = ',self.conf.get('pathConfig', 'outputFileName')
-        print u'[filterConfig]'
-        print u'filterTitle = ',self.conf.get('filterConfig', 'filterTitle')
-        print u'filterBuyerTitle = ',self.conf.get('filterConfig', 'filterBuyerTitle')     
-        print u'filterBuyerName = ',self.conf.get('filterConfig', 'filterBuyerName')
-        print u'filterBrandTitle = ',self.conf.get('filterConfig', 'filterBrandTitle')
-        print u'filterBrandName = ',self.conf.get('filterConfig', 'filterBrandName')
-        print u'filterCountTitle = ',self.conf.get('filterConfig', 'filterCountTitle')
-        print u'filterCountName = ',self.conf.get('filterConfig', 'filterCountName')
+        print u'[路径配置]'
+        print u'inputDir = ',self.conf.get(u'路径配置'.encode('utf-8'), 'inputDir').encode('gbk')
+        print u'outputDir = ',self.conf.get(u'路径配置'.encode('utf-8'), 'outputDir').encode('gbk')
+        print u'inputFileName = ',self.conf.get('路径配置', 'inputFileName').encode('gbk')
+        print u'outputFileName = ',self.conf.get('路径配置', 'outputFileName').encode('gbk')
+        print ''
+        print u'[过滤配置]'
+        print u'filterTitle = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterTitle').encode('gbk')
+        print u'filterBuyerTitle = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterBuyerTitle').encode('gbk')
+        print u'filterBuyerName = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterBuyerName').encode('gbk')
+        print u'filterBrandTitle = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterBrandTitle').encode('gbk')
+        print u'filterBrandName = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterBrandName').encode('gbk')
+        print u'filterCountTitle = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterCountTitle').encode('gbk')
+        print u'filterCountName = ',self.conf.get(u"过滤配置".encode('utf-8'), 'filterCountName').encode('gbk')
         print ''
 
     def saveSysConf(self):
